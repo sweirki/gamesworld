54301c018ffb6acba75c94f5c0f0a20d31198a6b
yaseen <michuae@yahoo.com>
2026-06-17 08:57:55 +0300

    Prepare iOS 4.2.7 release build

--- app.config.js
+++ app.config.js
@@ -90,6 +90,8 @@ export default ({ config }) => ({
       {
         ios: {
           useFrameworks: "static",
+          extraPods: [],
+          buildReactNativeFromSource: false,
         },
       },
     ],
